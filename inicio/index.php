a3a8cdf6b1eb0c411437b113db5e8cdcfccccf30
Optickal095 <eduardo.he095@gmail.com>
2021-10-12 16:14:24 -0300

    Crear_Publicación
    Botón y Perfil

--- inicio/index.php
+++ inicio/index.php
@@ -30,7 +30,7 @@
                                 <p class="lead text-muted">Revisa las últimas publicaciones</p>
                             </div>
                             <div class="col-lg-3 col-md-4 d-flex">
-                                <button class="btn btn-info w-100 align-self-center">Agregar publicacion</button>
+                                <button class="btn btn-info w-100 align-self-center form-control" data-toggle="modal" data-target="#agregarPublicacion">Agregar publicacion</button>
                             </div>
                         </div>
                     </div>
@@ -104,17 +104,48 @@
                                         reader.readAsDataURL(event.target.files[0]);
                                     };
                                 </script>
+                            </div>                            
+                        </div>
+                        <br><div class="row">
+                            <div class="form-group col-3">
+                                <input type="text" id="info_post" class="form-control" name="info_post" placeholder="Descripción" required>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="form-group col-3">
+                                <input type="text" id="precio" class="form-control" name="precio" placeholder="Precio" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-3">
+                                <input type="text" id="contacto" class="form-control" name="contacto" placeholder="Contacto" required>
+                            </div>
+                        </div>
+
+                        <div class="offset-10">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+        <script src="https://code.iconify.design/2/2.0.3/iconify.min.js%22%3E</script>
 </body>
 
 </html>
